41cce2b78d9944152ea6345aebba604ca1a2962b
Vinayak Chinchakhandi <vinayakchinchakhandi165@gmail.com>
2026-02-22 15:04:50 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
-import ScrollToTop from "./components/layout/ScrollToTop";
+import ScrollToTop from "./components/layout/ScrolltoTop";
 
 import Home from "./pages/Home";
 import ServiceDetails from "./pages/services/ServiceDetails";
