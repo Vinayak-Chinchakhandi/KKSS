6cf72fa800d106da45fdb33942ad722d6926a8e1
Vinayak Chinchakhandi <vinayakchinchakhandi165@gmail.com>
2026-02-25 17:49:40 +0530

    added map boeders

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -132,7 +132,7 @@ ${message}
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3840.3101067622633!2d75.9650704264641!3d15.734725949708611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb8694e290b3f53%3A0xfcb9c7ebb00bc9ad!2sKalakaleshwar%20Shamiyan%20Suppliers!5e0!3m2!1sen!2sin!4v1771700945084!5m2!1sen!2sin"
           width="100%"
           height="350"
-          style={{ border: 0, borderRadius: "12px" }}
+          className="map-frame"
           loading="lazy"
           allowFullScreen
         ></iframe>
